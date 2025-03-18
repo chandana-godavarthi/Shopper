a2123e5e8f4dfc89f6123f10e50614d388072ef2
chandana-godavarthi <sirichandana1069@gmail.com>
2025-03-18 18:29:52 +0530

    fix: correct property name from 'mongourl' to 'mongoUrl' in session store configuration

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ server.use(
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      mongourl: uri,
+      mongoUrl: uri,
       collectionName: "sessions",
     }),
     cookie: {
