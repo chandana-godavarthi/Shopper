2fe981a516c488a6333f371f74dda416089b8d13
vikhil1912 <vikhil1912@gmail.com>
2025-03-18 14:01:02 +0530

    setup build

--- shopper/vite.config.js
+++ shopper/vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // Ensures relative paths in production
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(), tailwindcss()],
   server: {
     port: 4000,
